0f9597c4e1611de47b74f8c55d3611d027de0383
Maudie <whippou@gmail.com>
2017-11-10 10:48:47 -0500

    Implemented seed and sequence loop

--- TP3Q1.py
+++ TP3Q1.py
@@ -2,6 +2,7 @@ import argparse
 
 def buildKmer(k,input):
     kmerList=[]
+    kmerPos=[]
     if k>0:
         # Parcourt la sequence
         for i in range(len(input) - k + 1):
@@ -11,6 +12,7 @@ def buildKmer(k,input):
                 temp += input[i + j]
             # On ajoute chaque kmer individuellement
             kmerList.append(temp)
+	    kmerPos.append(i)
     return kmerList
 
 def makeParser():
@@ -27,28 +29,36 @@ def makeParser():
 def makeSeed(k):
     temp=""
     for i in range(k):
-        temp+=1
+        temp+="1"
     return temp
 
-def findHSP(kmerList,seqDB):
-    compteurSeq=0
-    compteurKmer=0
+def findHSP(kmerList,seqDB, seed):
+    compteurPos=0
 
-    #TODO: Rajoute le loop dans la sequence
     #Pour chaque sous-mot
     for kmer in kmerList:
-        #On loop sur la longueur du sous-mot
-        for char in range(len(kmer)):
-            #TODO:Rajouter la prise en charge du 1 et du 0 dans le seed
-            #Si le caractere est le meme dans le sous mot et la sequence
-            if char == seqDB[compteurSeq]:
-                #On continue d'incrementer dans le sous mot
-                char += 1
-            else:
-                #On ressaiera a partir du debut au prochain caractere
-                char = 0
-            #On incremente la sequence totale
-            compteurSeq += 1
+	compteurPos = 0
+	compteurSeed = 0
+        #On loop sur la longueur de la sequence
+        for compteurSeq in range(len(seqDB)):
+            if seed[compteurSeed] == 1:
+		    #Si le caractere est le meme dans le sous mot et la sequence
+		    if kmer[compteurPos] == seqDB[compteurSeq]:
+			#On continue d'incrementer dans le sous mot
+			compteurPos += 1
+		    #Sinon, on verifie le premier caractere pour repartir une sous boucle
+		    elif kmer[0] == seqDB[compteurSeq]:
+			charPos = 1
+		    #Si rien ne match, on repart du premier caractere
+		    else :
+			charPos = 0
+		    #On incremente la sequence totale
+		    compteurSeq += 1
+	    else:
+		    #On a un zero, free pass
+		    compteurSeq += 1
+		    charPos += 1
+	    compteurSeed+=1
 
 def main():
     k = 5
