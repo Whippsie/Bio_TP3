a4a875a7b0422d3500a917750bfb223796606477
Maudee <maude.sabourin@umontreal.ca>
2017-11-18 12:21:02 -0500

    Removed numpy

--- TP3Q1.py
+++ TP3Q1.py
@@ -2,18 +2,20 @@
 # encoding: utf-8
 
 import argparse
-import numpy
+import math
 
 seuil = 4
-match,mismatch,indel=1,-1,-1
+match, mismatch, indel = 1, -1, -1
+
 
 def fastaSequences(filename):
-  sequences = []
-  with open(filename, 'r') as f:
-    for line in f:
-      if not '>' in line[0]:
-		sequences.append(line.strip("\n"))      
-  return sequences
+    sequences = []
+    with open(filename, 'r') as f:
+        for line in f:
+            if '>' not in line[0]:
+                sequences.append(line.strip("\n"))
+    return sequences
+
 
 def buildKmer(k, inputUser):
     kmerList = []
@@ -67,23 +69,23 @@ def findHSP(kmerList, seqDB, seed):
                         # On a un mot!
                         hspList.append(kmer)
                         hspPosDB.append(compteurSeq - len(kmer) + 1)
-                        if compteurSeq == (len(seqDB)-1):
+                        if compteurSeq == (len(seqDB) - 1):
                             compteurSeq += 1
                         else:
                             temp = (compteurSeq - compteurPos) + 1
-                            if temp == (compteurSeq-len(kmer))+1:
+                            if temp == (compteurSeq - len(kmer)) + 1:
                                 compteurSeq = temp + 1
                             else:
                                 compteurSeq = temp
                             compteurPos = 0
-                        # On continue d'incrementer dans le sous mot
+                            # On continue d'incrementer dans le sous mot
                     else:
                         compteurPos += 1
                         compteurSeq += 1
                 # Sinon, on verifie le premier caractere pour repartir une sous boucle
                 else:
                     if compteurPos > 0:
-                        if compteurSeq == (len(seqDB)-1):
+                        if compteurSeq == (len(seqDB) - 1):
                             compteurSeq += 1
                         else:
                             if compteurPos > 1:
@@ -110,7 +112,7 @@ def extendGlouton(kmerList, hspList, kmerPos, seqDB, seq, hspPos):
     for hsp in hspList:
         rightString = ""
         leftString = ""
-        #TODO: Probleme si plus qu'un HSP dans la sequence, nous donnera toujours le meme
+        # TODO: Probleme si plus qu'un HSP dans la sequence, nous donnera toujours le meme
         posDB = hspList.index(hsp)
         posDB = hspPos[posDB]
         maxScore = len(hsp) * 5
@@ -169,7 +171,7 @@ def extendGlouton(kmerList, hspList, kmerPos, seqDB, seq, hspPos):
         hspScoreList.append(maxScore)
     for hsp in hspExtendedList:
         print ("hspExtended:", hsp.hspString)
-    return hspExtendedList,hspScoreList
+    return hspExtendedList, hspScoreList
 
 
 def isEnd(currentHSP, seq, seqDB):
@@ -181,10 +183,11 @@ def isEnd(currentHSP, seq, seqDB):
 def bellowSeuil(maxScore, currentScore):
     return (maxScore - seuil) >= currentScore
 
+
 def merge(hspExtendedList, seqInput):
     i = 0
     while i < (len(hspExtendedList) - 1):
-        for j in range((i+1), len(hspExtendedList)):
+        for j in range((i + 1), len(hspExtendedList)):
             hsp1 = hspExtendedList[i]
             hsp2 = hspExtendedList[j]
             if (hsp1.seqStart - hsp2.seqStart) == (hsp1.dbStart - hsp2.dbStart):
@@ -202,6 +205,7 @@ def merge(hspExtendedList, seqInput):
         print ("hspMerged:", hsp.hspString)
     return hspExtendedList
 
+
 class Hsp:
     def __init__(self, hspString, seqStart, seqEnd, dbStart, dbEnd):
         self.hspString = hspString
@@ -210,82 +214,90 @@ class Hsp:
         self.dbStart = dbStart
         self.dbEnd = dbEnd
 
+
 def filterHSP(hspList, hspScoreList, l1, ldb):
     i = 0
     for hsp in hspList:
         bitscore = calcBitScore(hspScoreList[i])
-        calcEValue(l1,ldb,bitscore)
+        calcEValue(l1, ldb, bitscore)
         i += 1
-        
+
+
 def calcBitScore(scoreBrut):
     lambd = 0.192
     k = 0.176
-    return round(((lambd*scoreBrut)-numpy.log(k)/(numpy.log(2))), 6)
+    return round(((lambd * scoreBrut) - math.log(k) / (math.log(2))), 6)
 
 
 def calcEValue(lseq1, lseqdb, bitscore):
-    return lseq1*lseqdb*(numpy.power(2, -bitscore))
+    return lseq1 * lseqdb * (math.pow(2, -bitscore))
+
 
 def getLengthSeqDB(seqSearchDB):
     lgt = 0
     for seq in seqSearchDB:
-         lgt += len(seq)
+        lgt += len(seq)
     return lgt
+
 
 # CODE TP1
 def alignLocal(seq1, seqDB):
-    matrix, scoreMax, scoreMaxPos = makeMatrix(seq1,seqDB)
+    matrix, scoreMax, scoreMaxPos = makeMatrix(seq1, seqDB)
     print(matrix)
     print(scoreMax)
     path, end = sequencePath(matrix, scoreMaxPos, seq1, seqDB)
     aligned, alignValue = alignSequences(end, path, seq1, seqDB, scoreMaxPos, matrix.shape)
     print(aligned)
-    print("Alignement value:",alignValue)
+    print("Alignement value:", alignValue)
 
-def makeMatrix (seq1, seqDB):
-    matrix = numpy.zeros(shape=(len(seq1)+1,len(seqDB)+1))
+
+def makeMatrix(seq1, seqDB):
+    matrix = [[0 for col in range(len(seq1)+1)] for row in range(len(seqDB+1))]
     shape = matrix.shape
-    indel=-1
+    indel = -1
     scoreMax = 0
-    scoreMaxPos = (0,0)
-    for i in range(1,shape[0]):
-        for j in range(1,shape[1]):
+    scoreMaxPos = (0, 0)
+    for i in range(1, shape[0]):
+        for j in range(1, shape[1]):
             score = getScore(seq1, seqDB, i, j, matrix)
             if checkMax(score, scoreMax):
                 scoreMax = score
-                scoreMaxPos = (i,j)
+                scoreMaxPos = (i, j)
     return matrix, scoreMax, scoreMaxPos
 
-def alignSequences(start, path, seq1, seq2, end, size):
 
+def alignSequences(start, path, seq1, seq2, end, size):
     path.reverse()
     temp = seq2
     seq2 = seq1
     seq1 = temp
 
     """Indels de départ"""
-    seq1align,seq2align,x,y,z = genIndelStart(start,seq1,seq2)
+    seq1align, seq2align, x, y, z = genIndelStart(start, seq1, seq2)
 
     """Séquences de chemin"""
-    seq1align,seq2align,y,z,i = genSeqPath(seq1align,seq2align,path,y,z,seq1,seq2)
+    seq1align, seq2align, y, z, i = genSeqPath(seq1align, seq2align, path, y, z, seq1, seq2)
 
     """Complétion de la séquence"""
-    seq1align,seq2align= genIndelEnd(seq1align,seq2align,end,size,seq1,seq2)
+    seq1align, seq2align = genIndelEnd(seq1align, seq2align, end, size, seq1, seq2)
 
     return [seq1align, seq2align], i
 
-def getScore(seq1, seqDB, i,j,matrix):
-    if seq1[i-1] == seqDB[j-1]:
+
+def getScore(seq1, seqDB, i, j, matrix):
+    if seq1[i - 1] == seqDB[j - 1]:
         mymatch = 1
     else:
         mymatch = -1
     dscore = matrix[i - 1][j - 1] + mymatch
     uscore = matrix[i - 1][j] + indel
     lscore = matrix[i][j - 1] + indel
-    return max (0, dscore, uscore, lscore)
+    return max(0, dscore, uscore, lscore)
+
 
 def checkMax(score, scoreMax):
     return score > scoreMax
+
 
 def sequencePath(matrice, pos, seq1, seq2):
     x = pos[0]
@@ -294,22 +306,24 @@ def sequencePath(matrice, pos, seq1, seq2):
     current = matrice[x][y]
 
     while ((x > 0) and (y > 0)):
-        if ((current - mismatch == matrice[x-1][y-1]) and (seq1[x-1] != seq2[y-1])) or ((current - match == matrice[x-1][y-1]) and (seq1[x-1] == seq2[y-1])):
+        if ((current - mismatch == matrice[x - 1][y - 1]) and (seq1[x - 1] != seq2[y - 1])) or (
+            (current - match == matrice[x - 1][y - 1]) and (seq1[x - 1] == seq2[y - 1])):
             x -= 1
             y -= 1
-            path.append([1,1])
-        elif (current - indel == matrice[x-1][y]):
+            path.append([1, 1])
+        elif (current - indel == matrice[x - 1][y]):
             x -= 1
             current = matrice[x][y]
-            path.append([1,0])
-        elif (current - indel == matrice[x][y-1]):
+            path.append([1, 0])
+        elif (current - indel == matrice[x][y - 1]):
             y -= 1
             current = matrice[x][y]
-            path.append([0,1])
+            path.append([0, 1])
         else:
             break
         current = matrice[x][y]
-    return path, numpy.array([x,y])
+    #return path, numpy.array([x, y])
+    return path, None
 
 def genIndelStart(start, seq1, seq2):
     y = 0
@@ -332,62 +346,66 @@ def genIndelStart(start, seq1, seq2):
             z += 1
     else:
         pass
-    return seq1align, seq2align, x,y,z
+    return seq1align, seq2align, x, y, z
 
-def genSeqPath(s1,s2,p,y,z,sq1,sq2):
-  i = 0
-  while i < len(p):
-    if p[i][0] == 1:
-      s2 += sq2[y]
-      y += 1
-    else:
-      s2 += "-"
 
-    if p[i][1] == 1:
-      s1 += sq1[z]
-      z += 1
-    else:
-      s1 += "-"
-    i += 1
+def genSeqPath(s1, s2, p, y, z, sq1, sq2):
+    i = 0
+    while i < len(p):
+        if p[i][0] == 1:
+            s2 += sq2[y]
+            y += 1
+        else:
+            s2 += "-"
 
-  return s1,s2,y,z,i
+        if p[i][1] == 1:
+            s1 += sq1[z]
+            z += 1
+        else:
+            s1 += "-"
+        i += 1
 
-def genIndelEnd(s1,s2,end,size,sq1,sq2):
-  size_ligne = size[0] - 1
-  size_col = size[1] - 1
+    return s1, s2, y, z, i
 
-  if end[0] < size_ligne:
-    x = end[0]
-    while x < size_ligne:
-      s1 += "-"
-      s2 += sq2[x]
-      x += 1
-  elif end[1] < size_col:
-    x = end[1]
-    while x < size_col:
-      s2 += "-"
-      s1 += sq1[x]
-      x += 1
 
-  return s1,s2
+def genIndelEnd(s1, s2, end, size, sq1, sq2):
+    size_ligne = size[0] - 1
+    size_col = size[1] - 1
+
+    if end[0] < size_ligne:
+        x = end[0]
+        while x < size_ligne:
+            s1 += "-"
+            s2 += sq2[x]
+            x += 1
+    elif end[1] < size_col:
+        x = end[1]
+        while x < size_col:
+            s2 += "-"
+            s1 += sq1[x]
+            x += 1
+
+    return s1, s2
+
 
 def reverseSeq(seq):
-  seqrev=""
-  for s in seq:
-    if s=="A":
-      seqrev+="T"
-    elif s=="C":
-      seqrev+="G"
-    elif s=="T":
-      seqrev+="A"
-    elif s=="G":
-      seqrev+="C"
+    seqrev = ""
+    for s in seq:
+        if s == "A":
+            seqrev += "T"
+        elif s == "C":
+            seqrev += "G"
+        elif s == "T":
+            seqrev += "A"
+        elif s == "G":
+            seqrev += "C"
 
-  l=list(seqrev)
-  l.reverse()
-  seqrev =''.join(l)
+    l = list(seqrev)
+    l.reverse()
+    seqrev = ''.join(l)
 
-  return seqrev
+    return seqrev
+
 
 # CODE TP1
 
@@ -408,8 +426,8 @@ def main():
 
     seqSearchDB = fastaSequences("tRNAs.fasta")
     seqDB = seqSearchDB[0]
-    
-    print("seq input : ",seqInput)
+
+    print("seq input : ", seqInput)
     print("seq DB : ", seqDB)
 
     # MARCHE PAS POUR L'INSTANT
@@ -417,18 +435,19 @@ def main():
     kmerList, kmerPos = buildKmer(k, seqInput)
     seed = makeSeed(k)
     hspList, hspPos = findHSP(kmerList, seqDB, seed)
-    hspExtendedList,hspScoreList = extendGlouton(kmerList, hspList, kmerPos, seqDB, seqInput, hspPos)
+    hspExtendedList, hspScoreList = extendGlouton(kmerList, hspList, kmerPos, seqDB, seqInput, hspPos)
     hspMergedList = merge(hspExtendedList, seqInput)
 
-    #PAS FINI
+    # PAS FINI
     dbSeqLength = getLengthSeqDB(seqSearchDB)
     filterHSP(hspList, hspScoreList, len(seqInput), dbSeqLength)
     args = makeParser()
 
-    #NOTE : Args all treated as string
-    #if args.i:
-    #.format(args.square, answer)
+    # NOTE : Args all treated as string
+    # if args.i:
+    # .format(args.square, answer)
     # print(args.accumulate(args.integers))
+
 
 if __name__ == "__main__":
     main()
